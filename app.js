4fcfda6e5f62da244a5565f01f4664c1661f3981
aenia12 <aenia12@gmail.com>
2023-10-27 15:32:15 -0500

    Working on API integration.

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 let searchForm = document.querySelector("#search-form");
